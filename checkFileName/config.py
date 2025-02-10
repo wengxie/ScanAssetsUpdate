381eceb64c2f519f614d2f72ec6490107777aa38
kowen <15547715486@163.com>
2025-02-10 14:55:06 +0800

    修改提交

---
+++ checkFileName/config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# 指定要检查的目录路径
+# INBUNDLE_DIRECTORY = r'E:\trunkCHS\client\MainProject\Assets\InBundle'
+# EXCEL_PATH = r'E:\trunkCHS\datapool\ElementData\BaseData'
+
+INBUNDLE_DIRECTORY = r'E:\Dev_2024_3_30\client\MainProject\Assets\InBundle'
+EXCEL_PATH = r'E:\Dev_2024_3_30\datapool\ElementData\BaseData'
